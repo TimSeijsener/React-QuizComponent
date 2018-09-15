c94c489207e0a4ba9e8ff59e221f85001aed0dd5
GROUPINFRA\seijsenert <tim.seijsener@cgi.comm>
2018-09-15 11:55:41 +0200

    Completed the module2

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,7 +5,7 @@ import QuizEnd from './QuizEnd.js'
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
-  constructor (props){
+  constructor (props) {
     super(props)
     this.state = { quiz_position: 1 }
   }
@@ -16,11 +16,10 @@ class Quiz extends Component {
   }
 
   render(){
-    const isQuizEnd =((this.state.quiz_position -1) === quizData.quiz_questions.length) ===
-    quizData.quiz_questions.length)
+    const isQuizEnd =((this.state.quiz_position -1)   === quizData.quiz_questions.length) 
     return(
       <div>
-      {QuizEnd ? <QuizEnd /> :
+      {isQuizEnd ? <QuizEnd /> :
           <QuizQuestion quiz_question= {quizData.quiz_questions[this.state.quiz_position -1]} /> }
           showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
       </div>

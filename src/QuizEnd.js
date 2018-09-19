276a0d9eee62af6073533d436def42353827a742
GROUPINFRA\seijsenert <tim.seijsener@cgi.comm>
2018-09-19 11:50:35 +0200

    Completed the module2

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -9,11 +9,11 @@ render(){
   return (
   <div>
   <p>Thanks for playing!</p>
-  <a href=''> onClick={this.handleResetClick.bind(this)}> Reset Quiz</a>
+  <a href='' onClick={this.handleResetClick.bind(this)}> Reset Quiz</a>
   </div>
 
   )
 }
 }
 
-export default QuizEnd
+export default QuizEnd;
